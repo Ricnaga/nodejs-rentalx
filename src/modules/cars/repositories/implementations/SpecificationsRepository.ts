92e5727e4911201568d8443fc6419a048203cf1a
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-05-18 19:13:50 -0300

    create category|specifications repository(liskov)

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,33 +1,32 @@
-import { Specification } from "../../model/Specification";
+import { getRepository, Repository } from "typeorm";
+
+import { Specification } from "../../entities/Specification";
 import {
     ICreateSpecificationDTO,
     ISpecificationsRepository,
 } from "../ISpecificationsRepository";
 
 export class SpecificationsRepository implements ISpecificationsRepository {
-    private specification: Specification[];
+    private repository: Repository<Specification>;
 
     constructor() {
-        this.specification = [];
+        this.repository = getRepository(Specification);
     }
 
-    create({ description, name }: ICreateSpecificationDTO): void {
-        const specification = new Specification();
-
-        Object.assign(specification, {
+    async create({
+        description,
+        name,
+    }: ICreateSpecificationDTO): Promise<void> {
+        const specification = this.repository.create({
             description,
             name,
-            created_at: new Date(),
         });
 
-        this.specification.push(specification);
+        await this.repository.save(specification);
     }
 
-    findByName(name: string): Specification {
-        const specification = this.specification.find(
-            (specification) => specification.name === name
-        );
-
+    async findByName(name: string): Promise<Specification> {
+        const specification = await this.repository.findOne({ name });
         return specification;
     }
 }
