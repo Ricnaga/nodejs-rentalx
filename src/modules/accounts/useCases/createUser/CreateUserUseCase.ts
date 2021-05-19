ce6b862d2a6ca304307b34a78b49eb7f323b852d
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-05-19 13:50:55 -0300

    create services|controllers for users

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,36 @@
+import { hash } from "bcrypt";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUsersDTO } from "../../dtos/ICreateUsersDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+    async execute({
+        name,
+        password,
+        email,
+        driver_license,
+    }: ICreateUsersDTO): Promise<void> {
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if (userAlreadyExists) {
+            throw new AppError("User already exists");
+        }
+
+        const passwordHash = await hash(password, 8);
+        await this.usersRepository.create({
+            name,
+            password: passwordHash,
+            email,
+            driver_license,
+        });
+    }
+}
+
+export { CreateUserUseCase };
