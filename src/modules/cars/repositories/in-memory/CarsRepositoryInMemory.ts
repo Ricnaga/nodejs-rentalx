791a04175d54232693ebaa4efbd75371b75ba482
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-06-10 23:44:55 -0300

    create listing available cars controllers and use cases

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -32,4 +32,27 @@ export class CarsRepositoryInMemory implements ICarsRepository {
     async findByLicensePlate(license_plate: string): Promise<Car> {
         return this.cars.find((car) => car.license_plate === license_plate);
     }
+
+    async findAvailable(
+        category_id?: string,
+        brand?: string,
+        name?: string
+    ): Promise<Car[]> {
+        return this.cars.filter((car) => {
+            if (
+                car.available === true ||
+                (brand && car.brand === brand) ||
+                (category_id && car.category_id === category_id) ||
+                (name && car.name === name)
+            ) {
+                return car;
+            }
+
+            return null;
+        });
+    }
+
+    async findById(id: string): Promise<Car> {
+        return this.cars.find((car) => car.id === id);
+    }
 }
