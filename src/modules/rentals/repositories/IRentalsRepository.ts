99cbf9bc0f33fb08f7b4a03294513112cb16927d
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-06-13 15:59:59 -0300

    refatored rental: interfaces | migrations

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -3,6 +3,6 @@ import { Rental } from "../infra/typeorm/entities/Rental";
 
 export interface IRentalsRepository {
     findOpenRentalByCar(car_id: string): Promise<Rental>;
-    findOpenRentalByUser(user_id): Promise<Rental>;
+    findOpenRentalByUser(user_id: string): Promise<Rental>;
     create(data: ICreateRentalDTO): Promise<Rental>;
 }
